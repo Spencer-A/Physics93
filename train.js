8cb4fb4430b4921406de24b0dba03a910b1b5a92
Spencer-A <36409356+Spencer-A@users.noreply.github.com>
2019-03-08 18:53:00 -0500

    Add files via upload

--- train.js
+++ train.js
@@ -1,15 +1,24 @@
+
 $( document ).ready(function() {
     calculateW();
     drawDisk();
     drawTrain();
 });
 
+
+var timerId = -1;
+var startTime;
+var currentTime = 0;
+var diskAngularVelocity;
+var trainAngularVelocity;
+var Dm, Dr, Tm, Tr, Tv;
+
 function calculateW(){
-  var Dm = document.getElementById("DiskM").value;
-  var Dr = document.getElementById("DiskR").value;
-  var Tm = document.getElementById("TrainM").value;
-  var Tr = document.getElementById("TrainR").value;
-  var Tv = document.getElementById("TrainV").value;
+  Dm = document.getElementById("DiskM").value;
+  Dr = document.getElementById("DiskR").value;
+  Tm = document.getElementById("TrainM").value;
+  Tr = document.getElementById("TrainR").value;
+  Tv = document.getElementById("TrainV").value;
 
   console.log(Dm);
   console.log(Dr);
@@ -20,47 +29,88 @@ function calculateW(){
   var Ti = Tm*Tr*Tr;
   var Di = .5*Dm*Dr*Dr;
 
-  var result = (-1*(Ti)*(Tv/Tr))/((Di)+(Ti));
-  console.log(result);
+  diskAngularVelocity = (-1*(Ti)*(Tv/Tr))/((Di)+(Ti));
+  console.log(diskAngularVelocity);
   var output = document.getElementById("output");
-  output.value = result;
+  output.value = diskAngularVelocity;
+
+  trainAngularVelocity = Tv / Tr + diskAngularVelocity;
 }
 
 function drawTrain(){
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "#FF0000";
-  var width = 200;
-  var height = 100;
-  var x = Math.round(canvas.width/2 - width/2);
-  var y = Math.round(canvas.height/2 + canvas.height*3/4*document.getElementById("TrainR").value/document.getElementById("DiskR").value);
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);  // center in the canvs
 
-  console.log(x);
-  console.log(y);
-  console.log(width);
-  console.log(height);
-  //ctx.fillRect(10,0,100,100)
-  ctx.fillRect(Math.round(x),Math.round(y),Math.round(width),Math.round(height));
+  var angle = trainAngularVelocity * currentTime / 1000;
+  ctx.rotate(-angle);  // Rotate clockwise the angle
+
+  ctx.fillStyle = "#FF0000";
+  ctx.translate(0, canvas.height * 3/8 * Tr / Dr);
+  ctx.fillRect(-10,-5,20,10);
+
+  ctx.restore();
 }
 
 function drawDisk(){
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
+  // time is in milliseconds, so angle = omega * time / 1000;
+  var angle = currentTime * diskAngularVelocity / 1000;
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);  // Move the origin to the center of the canvas
+  ctx.rotate(-angle);  // Rotate clockwise the angle
+
   ctx.fillStyle = "#FFFF00";
   ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height/2, canvas.width*3/8, 0, Math.PI/2);
-  ctx.lineTo(canvas.width/2, canvas.height/8);
-  ctx.arc(canvas.width/2, canvas.height/2, canvas.width*3/8, Math.PI*3/2, Math.PI, true);
-  ctx.lineTo(canvas.width*7/8, canvas.height/2);
+  ctx.arc(0, 0, canvas.width*3/8, 0, Math.PI/2);
+  ctx.lineTo(0, 0);
+  ctx.arc(0, 0, canvas.width*3/8, Math.PI*3/2, Math.PI, true);
+  ctx.lineTo(0, 0);
   ctx.fill();
 
   ctx.fillStyle = "#0000FF";
   ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height/2, canvas.width*3/8, Math.PI/2, Math.PI);
-  ctx.lineTo(canvas.width*7/8, canvas.height/2);
-  ctx.arc(canvas.width/2, canvas.height/2, canvas.width*3/8, Math.PI*2, Math.PI*3/2, true);
-  ctx.lineTo(canvas.width/2, canvas.height*7/8);
+  ctx.arc(0, 0, canvas.width*3/8, Math.PI/2, Math.PI);
+  ctx.lineTo(0,0);
+  ctx.arc(0, 0, canvas.width*3/8, Math.PI*2, Math.PI*3/2, true);
+  ctx.lineTo(0,0);
   ctx.fill();
+
+  ctx.restore();
+}
+
+function drawCanvas() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawDisk();
+    drawTrain();
+}
+
+function updateTime() {
+    var d = new Date();
+    currentTime = d.getTime() - startTime;
+    drawCanvas();
+}
+
+function goButton() {
+//    console.log("Button pushed", timerId, document.getElementById("go"));
+    if (timerId == -1) {
+        calculateW();
+        timerId = window.setInterval(updateTime, 15);
+        document.getElementById('go').innerHTML = "Stop";
+        var d = new Date();
+        startTime = d.getTime();
+        currentTime = 0;
+    }
+    else {
+        window.clearInterval(timerId);
+        timerId = -1;
+        document.getElementById('go').innerHTML = "Go";
+    }
+
 }
